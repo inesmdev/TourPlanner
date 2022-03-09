da997abfdc8a4f19402ec444b48c4b32fe756b66
Ines M <80414584+xxaemy@users.noreply.github.com>
2022-03-09 02:17:38 +0100

    Initial Commit

---
+++ TourPlanner.Models/Tour.cs
@@ -0,0 +1,8 @@
+namespace TourPlanner.Models
+{
+    public class Tour
+    {
+        public string Tourname { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ TourPlanner.UI/ViewModels/BaseVM.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace TourPlanner.UI.ViewModels
+{
+    public abstract class BaseVM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChangedEvent([CallerMemberName] string propertyName = "")
+        {
+            ValidatePropertyName(propertyName);
+
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        protected void ValidatePropertyName(string propertyName)
+        {
+            if (TypeDescriptor.GetProperties(this)[propertyName] == null)
+            {
+                throw new ArgumentException("Invalid property name: " + propertyName);
+            }
+        }
+    }
+}
